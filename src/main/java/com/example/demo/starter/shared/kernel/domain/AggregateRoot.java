de93b7ae9c158a92cdfd0245d2352199b735606f
Onurcan Oğul <147406204+onrcanogul@users.noreply.github.com>
2025-11-29 14:34:38 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/starter/shared/kernel/domain/AggregateRoot.java
@@ -0,0 +1,23 @@
+package com.example.demo.starter.shared.kernel.domain;
+
+import com.example.demo.starter.shared.base.entity.BaseEntity;
+import com.example.demo.starter.shared.kernel.event.domain.DomainEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AggregateRoot extends BaseEntity {
+
+    private final List<DomainEvent> events = new ArrayList<>();
+
+    protected void registerEvent(DomainEvent event) {
+        events.add(event);
+    }
+
+    public List<DomainEvent> pullEvents() {
+        var copy = List.copyOf(events);
+        events.clear();
+        return copy;
+    }
+}
+
