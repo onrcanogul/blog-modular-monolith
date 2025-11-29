df2dc22978f44bfb4274a667580b3695e13a6972
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-11-29 15:05:42 +0300

    profile service has been implemented

---
+++ src/main/java/com/example/demo/starter/module/profile/package-info.java
@@ -0,0 +1,4 @@
+@ApplicationModule(allowedDependencies = { "com.example.demo.starter.auth" })
+package com.example.demo.starter.module.profile;
+
+import org.springframework.modulith.ApplicationModule;
