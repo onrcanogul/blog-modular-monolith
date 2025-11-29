459e9f627e772d2b13f8a46c704a2512343a3c16
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-11-29 16:37:31 +0300

    example module was deleted

--- src/main/java/com/example/demo/starter/module/auth/package-info.java
+++ src/main/java/com/example/demo/starter/module/auth/package-info.java
@@ -1,4 +1,4 @@
-@ApplicationModule(allowedDependencies = { "com.example.demo.starter.module1" })
+@ApplicationModule(allowedDependencies = { "com.example.demo.starter.profile" })
 package com.example.demo.starter.module.auth;
 
 import org.springframework.modulith.ApplicationModule;
